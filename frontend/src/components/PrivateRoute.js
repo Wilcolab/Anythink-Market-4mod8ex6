0c42f12f06de4a8573084c29e1d28c6632d9b2b5
Yehuda <yehudamber@gmail.com>
2023-03-23 20:06:58 +0000

    Make 'PrivateRoute' a class component

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -8,8 +8,11 @@ function Message() {
     return <div><h1>This is a private page</h1></div>
 }
 
-function PrivateRoute({ authenticated, component, ...rest }) {
-    return <Route {...rest} component={authenticated ? component : Message} />;
+class PrivateRoute extends React.Component {
+    render() {
+        const { authenticated, component, ...rest } = this.props;
+        return <Route {...rest} component={authenticated ? component : Message} />;
+    }
 }
 
 export default connect(mapStateToProps)(PrivateRoute);
