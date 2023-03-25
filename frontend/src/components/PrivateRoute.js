2e77a516577ae8bdb225a671301c698815c1171f
Yehuda <yehudamber@gmail.com>
2023-03-25 20:33:02 +0000

    Use 'currentUser'

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -2,14 +2,14 @@ import React from "react"
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
-const mapStateToProps = (state) => ({ authenticated: state.common.currentUser != null });
+const mapStateToProps = (state) => ({ currentUser: { isAuthenticated: state.common.currentUser != null } });
 
 function Message() {
     return <div><h1>This is a private page</h1></div>
 }
 
-function PrivateRoute({ authenticated, component, ...rest }) {
-    return <Route {...rest} component={authenticated ? component : Message} />;
+function PrivateRoute({ currentUser, component, ...rest }) {
+    return <Route {...rest} component={currentUser.isAuthenticated ? component : Message} />;
 }
 
 export default connect(mapStateToProps)(PrivateRoute);
