16c07208affda894591ce2c9fe8268120feecc64
Yehuda <yehudamber@gmail.com>
2023-03-25 20:39:38 +0000

    Pass 'currentUser' from App.js

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,15 +1,10 @@
 import React from "react"
 import { Route } from "react-router-dom";
-import { connect } from "react-redux";
-
-const mapStateToProps = (state) => ({ currentUser: { isAuthenticated: state.common.currentUser != null } });
 
 function Message() {
     return <div><h1>This is a private page</h1></div>
 }
 
-function PrivateRoute({ currentUser, component, ...rest }) {
+export default function PrivateRoute({ currentUser, component, ...rest }) {
     return <Route {...rest} component={currentUser.isAuthenticated ? component : Message} />;
 }
-
-export default connect(mapStateToProps)(PrivateRoute);
